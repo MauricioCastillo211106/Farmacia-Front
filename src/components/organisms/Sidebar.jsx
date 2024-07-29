bcfd420fc2fc098c431c9a9cb5c1ba53af0fd9ba
Mauricio David Castillo Ruvalcaba <211106@ids.upchiapas.edu.mx>
2024-07-29 01:24:52 -0600

    [Add] admin page sidebar navegation

--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Eliminar el token de autenticación
+    localStorage.removeItem('token');
+    // Redirigir al usuario a la página de inicio de sesión o principal
+    navigate('/login');
+  };
+
   return (
     <div className="sidebar">
       <h2>Zona Administrativa</h2>
       <div className="sidebar-section">
         <h3>Inventario</h3>
         <ul>
-          <li><Link to="add-product">Agregar Producto</Link></li>
-          <li><Link to="delete-product">Eliminar Producto</Link></li>
-          <li><Link to="edit-product">Editar Producto</Link></li>
+          <li><Link to="/add-product">Agregar Producto</Link></li>
+          <li><Link to="/delete-product">Eliminar Producto</Link></li>
+          <li><Link to="/edit-product">Editar Producto</Link></li>
         </ul>
       </div>
       <div className="sidebar-section">
@@ -27,6 +36,13 @@ const Sidebar = () => {
           <li><Link to="#">Agregar Empleado</Link></li>
           <li><Link to="#">Eliminar Empleado</Link></li>
           <li><Link to="#">Editar Empleado</Link></li>
+        </ul>
+      </div>
+      <div className="sidebar-section">
+        <h3>Navegación</h3>
+        <ul>
+          <li><a href="/login" onClick={handleLogout}>Cerrar Sesión</a></li>
+          <li><Link to="/">Página Principal</Link></li>
         </ul>
       </div>
     </div>
